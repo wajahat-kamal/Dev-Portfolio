9b512d0a31bb30892af5e075717587dec9abb20c
shariq-yousuf <shariqyousuf01@gmail.com>
2025-05-24 17:31:00 +0500

    first commit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,4 @@
-import { ArrowDown } from "lucide-react";
+import { ArrowDown, Github } from "lucide-react";
 
 export const HeroSection = () => <section id="hero" className="relative min-h-screen flex flex-col items-center justify-center px-4">
 
@@ -29,16 +29,22 @@ export const HeroSection = () => <section id="hero" className="relative min-h-sc
         Wordpress and ReactJS Developer
       </p>
 
-      <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+      <div className=" opacity-0 animate-fade-in-delay-4">
         <a href="#projects" className="cosmic-button">
           View My Work
         </a>
       </div>
+      <div className=" opacity-0 animate-fade-in-delay-4">
+        <a target="-blank" href="https://github.com/wajahat-kamal" className="cosmic-button">
+          <Github/>
+        </a>
+      </div>
+
     </div>
 
   </div>
 
-  <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+  <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
     <span className="text-sm text-muted-foreground mb-2"> Scroll </span>
     <ArrowDown className="h-5 w-5 text-primary" />
   </div>
