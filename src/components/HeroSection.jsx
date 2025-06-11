eec6d39e7a4f4cd43c079baac38d47468938d7b9
shariq-yousuf <shariqyousuf01@gmail.com>
2025-06-11 09:42:20 +0500

    Uncomment scroll prompt in HeroSection for improved user navigation

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -45,9 +45,9 @@ export const HeroSection = () => (
       </div>
     </div>
 
-    {/* <div className="absolute bottom-1 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+    <div className="absolute bottom-1 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
       <span className="text-sm text-muted-foreground mb-2"> Scroll </span>
       <ArrowDown className="h-5 w-5 text-primary" />
-    </div> */}
+    </div>
   </section>
 );
