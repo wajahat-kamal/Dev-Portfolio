dd4aa50d02a16d3d7a0324823678d3e761be7cac
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-06-11 10:01:01 +0500

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,36 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PageLoader from "./components/PageLoader";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
-import React, { useEffect, useState } from 'react';
-import PageLoader from './components/PageLoader';
 
 function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 3000); 
+    }, 3000);
 
     return () => clearTimeout(timer);
   }, []);
 
-
   return (
-
-  
-      <>
+    <>
       {loading ? (
         <PageLoader />
       ) : (
         <div className="p-0">
-          
-      <BrowserRouter>
-        <Routes>
-
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-
-        </Routes>
-      </BrowserRouter>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
         </div>
       )}
     </>
-
   );
 }
-
-
-
-
 
 export default App;
