a61866e35662a4ca357542146e38621e4000335a
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-07-05 13:02:07 +0500

    Updating Hero  Section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
         {/* Left Content */}
         <div className="text-start lg:text-left flex-1 space-y-2 md:space-y-5">
           {/* Heading */}
-          <h1 className="text-3xl sm:text-5xl md:text-6xl font-extrabold tracking-tight">
+          <h1 className="text-[32px] sm:text-5xl md:text-6xl font-extrabold tracking-tight">
             <span className="block opacity-0 animate-fade-in text-white text-center md:text-start text-2xl md:text-4xl">
               Hi, I'm
             </span>
@@ -28,7 +28,7 @@ const HeroSection = () => {
           {/* Buttons */}
           <div className="flex flex-wrap items-center justify-center lg:justify-start gap-1 md:gap-4 opacity-0 animate-fade-in-delay-4">
             {/* CV Button */}
-            <a className="px-4 py-2 bg-primary text-white rounded-full font-semibold hover:text-primary hover:bg-transparent transition duration-300 hover:border-primary border-2 cursor-pointer">
+            <a className="md:px-6 px-4 py-2 bg-primary text-white rounded-full font-semibold hover:text-primary hover:bg-transparent transition duration-300 hover:border-primary border-2 cursor-pointer">
               Download CV
             </a>
 
@@ -36,11 +36,11 @@ const HeroSection = () => {
             <a
               target="_blank"
               href="https://github.com/wajahat-kamal"
-              className="flex items-center  hover:shadow-sm shadow-primary gap-2 px-2 py-[6px] md:px-3  md:py-[8px] border-2 border-primary text-primary rounded-full hover:bg-primary  hover:text-white transition duration-300"
+              className="flex items-center  hover:shadow-sm shadow-primary gap-2 px-2 py-[6px] md:px-2 md:py-[6px] border-2 border-primary text-primary rounded-full hover:bg-primary  hover:text-white transition duration-300"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 md:w-[22px] md-[22px]"
+                className="w-6 h-6 md:w-8 md-8"
                 viewBox="0 0 24 24"
                 fill="currentColor"
               >
@@ -67,13 +67,13 @@ const HeroSection = () => {
             <a
               target="_blank"
               href="https://www.linkedin.com"
-              className="flex items-center gap-2 hover:shadow-sm shadow-primary px-2 py-[8px] md:px-3  md:py-[8px] border-2 border-primary text-primary rounded-full hover:bg-primary hover:text-white transition duration-300"
+              className="flex items-center gap-2 hover:shadow-sm shadow-primary px-2 py-[8px] md:px-2  md:py-[8px] border-2 border-primary text-primary rounded-full hover:bg-primary hover:text-white transition duration-300"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-5 h-5 md:w-[22px] md-[22px]"
+                className="w-5 h-5 md:w-8 md-8"
               >
                 <path
                   d="M19 0h-14C2.239 0 0 2.239 0 5v14c0 2.761 
