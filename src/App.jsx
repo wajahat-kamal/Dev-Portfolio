97300773eaade8bc02a5d97a7fb77afd3ccaa22d
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-08-01 11:59:24 +0500

    Update loading timer in App component to 1500ms and modify greeting text in HeroSection from "Hi" to "Hey" for a more casual tone.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2400);
+    }, 1500);
 
     return () => clearTimeout(timer);
   }, []);
