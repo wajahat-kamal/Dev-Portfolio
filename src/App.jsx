fa4b72de02c1d2e3d26bffeafb0a4aeda8ac5ff0
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-01-10 10:34:30 +0500

    Refactor App component: remove loading state and PageLoader component to streamline rendering and improve initial load performance.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PageLoader from "./components/PageLoader";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 500);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <>
-      {loading ? (
-        <PageLoader />
-      ) : (
-        <div className="p-0">
-          <BrowserRouter>
-            <Routes>
-              <Route index element={<Home />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </div>
-      )}
+      <div className="p-0">
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
