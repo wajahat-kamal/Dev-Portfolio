0216f05b8759b9640123dcd4630d92e513af432d
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-01-10 19:42:32 +0500

    Revamp Footer component: update layout and styling for improved visual appeal and clarity, including new background and text styles.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,9 @@
-
 export const Footer = () => {
   return (
-    <footer className="text-foreground py-3 sm:py-4 px-3 sm:px-20 bg-[#0B111E] flex justify-center items-center gap-2 sm:gap-0 text-center sm:text-left">
-      {/* Left Section */}
-      <div className="text-center">
-        <p className="text-xs sm:text-sm">
-          &copy; {new Date().getFullYear()} Wajahat Kamal 27-July. All rights reserved.
-        </p>
-        <p className="text-xs sm:text-sm">
-          Built with{" "}
-          <a
-            target="_blank"
-            href="https://reactjs.org"
-            className="text-blue-500 hover:underline"
-          >
-            React JS
-          </a>{" "}
-          and{" "}
-          <a
-            target="_blank"
-            href="https://tailwindcss.com"
-            className="text-blue-500 hover:underline"
-          >
-            Tailwind CSS
-          </a>
-          .
-        </p>
+    <footer className="w-full py-4 border border-zinc-200/60 dark:border-zinc-800/60 bg-white/60 dark:bg-zinc-900/40 backdrop-blur-xl">
+      <div className="text-center text-zinc-600 font-bold dark:text-zinc-400 text-[16px]">
+        &copy; {new Date().getFullYear()} Wajahat Kamal | Full Stack Developer
       </div>
-
-     
     </footer>
   );
 };
