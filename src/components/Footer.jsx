7ae6b6cf83df1057b7c0be86fa16b49d99263476
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-01-10 19:44:03 +0500

    Refactor Footer component: update styling to enhance visual appeal by adjusting border and background colors for a more cohesive design.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="w-full py-4 border border-zinc-200/60 dark:border-zinc-800/60 bg-white/60 dark:bg-zinc-900/40 backdrop-blur-xl">
+    <footer className="w-full py-5 border border-zinc-800/60 bg-zinc-900/40 backdrop-blur-xl">
       <div className="text-center text-zinc-600 font-bold dark:text-zinc-400 text-[16px]">
         &copy; {new Date().getFullYear()} Wajahat Kamal | Full Stack Developer
       </div>
