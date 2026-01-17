950252d362120832864cc1b4dbe9bb97d09e336c
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-01-17 15:30:22 +0500

    Update Footer styling: change background color for improved visual appeal and consistency.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="w-full py-5 border border-zinc-800/60 bg-zinc-900/40 backdrop-blur-xl">
+    <footer className="w-full py-5 border border-zinc-800/60 bg-[#0A0F1C] backdrop-blur-xl">
       <div className="text-center text-zinc-600 font-bold dark:text-zinc-400 text-[16px]">
         &copy; {new Date().getFullYear()} Wajahat Kamal | Full Stack Developer
       </div>
