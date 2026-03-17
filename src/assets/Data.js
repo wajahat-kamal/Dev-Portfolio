406d06eec1a8a6eaf3676fb2f086d3a6f8c0394f
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-17 22:32:58 +0500

    Update Data.js: add Redux icon import to enhance project skill representation.

--- src/assets/Data.js
+++ src/assets/Data.js
@@ -10,6 +10,7 @@ import mongodbIcon from "./skills/mongodb-icon.svg";
 import expressjsIcon from "./skills/express-icon.png";
 import nodejsIcon from "./skills/Nodejs-icon.svg";
 import reactIcon from "./skills/React-icon.svg";
+import reduxIcon from "./skills/redux-icon.svg";
 import typescriptIcon from "./skills/typescript-icon.svg";
 import javascriptIcon from "./skills/javascript-icon.svg";
 import pythonIcon from "./skills/python-icon.png"
