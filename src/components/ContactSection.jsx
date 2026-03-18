202755c9dc4abac300de1e9aef12e49449ddea39
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-03-18 22:59:43 +0500

    Enhance ContactSection: add hover underline effect to contact links for improved user interaction and visual feedback.

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -155,7 +155,7 @@ export const ContactSection = () => {
               <motion.a
                 key={i}
                 href={obj.url}
-                className="flex items-center gap-4 text-sm text-zinc-400"
+                className="flex items-center gap-4 text-sm text-zinc-400 hover:underline"
                 variants={{
                   hidden: { opacity: 0, x: -18, filter: "blur(4px)" },
                   show: { opacity: 1, x: 0, filter: "blur(0px)" },
